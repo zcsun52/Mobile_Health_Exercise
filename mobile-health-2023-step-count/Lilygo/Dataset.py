36f043e930979fe11d05b649aa7ed4635d19ef74
zcsun52 <zcsun@stanford.edu>
2023-04-27 14:51:04 +0200

    clean up

--- mobile-health-2023-step-count/Lilygo/Dataset.py
+++ mobile-health-2023-step-count/Lilygo/Dataset.py
@@ -2,36 +2,47 @@ import json
 import numpy as np
 from scipy import interpolate
 
-class Dataset:    
-    # name: used for axis names
-    # values: list of all sensor values in chronological order
-    # timestamps: list of tuples (data_index, timestamp)
+class Dataset:
+    """
+    A class representing a collection of sensor readings.
+
+    Attributes:
+        title (str): Title for this collection of data.
+        values (list): List of corresponding sensor values in chronological order.
+        raw_timestamps (list): List of tuples (data_index, timestamp) written for every packet, also if no data for this collection was added; data_index may be repeated (last entry counts); timestamp in milliseconds.
+        total_time (float): Time in seconds between start and end of recording (i.e., length of recording).
+        timestamps (numpy.ndarray): Timestamp offset for each sensor value (since start of recording in seconds) obtained by linearly interpolating between the start and end of the recording, interpolates also over sampling gaps (i.e., timestamps are potentially inaccurate in case a sensor fails to report values for longer periods of time).
+        samplerate (float): Mean sample rate over the whole duration of the recording in samples/s.
+        _update_timestamps (list): Timestamps for values with consecutive duplicate entries removed.
+        _update_values (list): Sensor values with consecutive duplicate entries removed.
+        _update_idxs (list): List indices for values with consecutive duplicate entries removed.
+        _max_update_gap (float): Maximum duration in seconds for which the sensor value does not change.
+        _int_timestamps (numpy.ndarray): Interpolated timestamp offset for each sensor value (since start of recording in seconds) obtained by interpolating between raw timestamps, should be correct also in case a sensor stops reporting values over a segment of the recording but individual sampling intervals might vary.
+
+    Methods:
+        fromLists(cls, name, values, timestamps): Create a new Dataset object from the given name, values, and timestamp offsets.
+        update_timestamps(self): Get the update timestamps.
+        update_values(self): Get the update values.
+        update_idxs(self): Get the update indices.
+        max_update_gap(self): Get the maximum update gap.
+        int_timestamps(self): Get the interpolated timestamps.
+
+    """
+
     def __init__(self, name, values, timestamps):
-        self.title = name # Title for this collections of data
-        self.values = values # list of numpy time series (for example [ax, ay az])
-        self.raw_timestamps = timestamps # for plotting and processing, list of numpy array of timestamps for the axis
-        self._update_values = None # all values with consecutive duplicate entries removed
-        self._update_timestamps = None # timestamps for values with consecutive duplicate entries removed
-        self._update_idxs = None # list idx for values with consecutive duplicate entries removed
-
-        # calculate interpolated timestamps 
-        # timestamps go linearly from first time to last time
-        # seconds from start
-        self._int_timestamps = None 
-
-        # seconds between first and last sample
-        self.total_time = (self.raw_timestamps[-1][1] - self.raw_timestamps[0][1]) / 1000
-        # linearly interpolated offset in seconds from the first sample
+        self.title = name
+        self.values = values
+        self.raw_timestamps = timestamps
+        self.total_time = (self.raw_timestamps[-1][1] - self.raw_timestamps[0][1]) / 1000 
         self.timestamps = np.linspace(0, self.total_time, num=len(values))
+        self.samplerate = len(self.values) / self.total_time
+        self._update_timestamps = None
+        self._update_values = None
+        self._update_idxs = None
+        self._max_update_gap = None
+        self._int_timestamps = None
         
-        # Calculate average sample rate
-        # samples/s
-        self.samplerate = len(self.values) / self.total_time 
 
-        self._max_update_gap = None     
-        
-    #skip raw timestamp conversion
-    #directly pass values and corresponding timestamps
     @classmethod
     def fromLists(cls, name, values, timestamps):
         raw_timestamps = [(n, 1000 * ts) for n, ts in zip(range(len(timestamps)), timestamps)]
@@ -54,23 +65,34 @@ class Dataset:
         if self._update_idxs is None:
             vals = np.array(self.values, dtype=float)
             val_diffs = np.diff(vals, prepend=vals[0]-1)
-            self._update_idxs = np.where(val_diffs > 0)[0]
+            self._update_idxs = np.where(val_diffs != 0)[0].tolist()
         return self._update_idxs
 
     @property
     def max_update_gap(self):
         if self._max_update_gap is None:
-            self._max_update_gap = max(np.diff(self.update_timestamps) / 1000) if len(self.update_timestamps) > 1 else np.inf
+            timestamps = self.update_timestamps.copy()
+            if self.update_timestamps[0] > 0: # if first sensor value was captured after start of recording
+                timestamps = [0,] + timestamps
+            if self.update_timestamps[-1] < self.total_time: # if last sensor update happened before end of recording
+                timestamps.append(self.total_time)
+            self._max_update_gap = max(np.diff(timestamps)) if len(timestamps) > 1 else np.inf
         return self._max_update_gap
 
     @property
     def int_timestamps(self):
         if self._int_timestamps is None:
-            idxs = [rts[0] for rts in self.raw_timestamps]
-            ts = [rts[1] for rts in self.raw_timestamps]
+            idxs = []
+            ts = []
+            assert len(self.raw_timestamps)>1
+            for rts, next_rts in zip(self.raw_timestamps[:-1], self.raw_timestamps[1:]):
+                if rts[0] != next_rts[0]:
+                    idxs.append(rts[0])
+                    ts.append(rts[1])
 
-            self._int_timestamps = interpolate.interp1d(idxs, ts, fill_value="extrapolate")(np.arange(len(self.values)))
+            if len(self.values)>self.raw_timestamps[-1][0]:
+                idxs.append(self.raw_timestamps[-1][0])
+                ts.append(self.raw_timestamps[-1][1])
+
+            self._int_timestamps = (interpolate.interp1d(idxs, ts, fill_value="extrapolate")(np.arange(len(self.values))) - self.raw_timestamps[0][1]) / 1000
         return self._int_timestamps
-
-
-    
